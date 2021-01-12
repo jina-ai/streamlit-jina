1a1e79be55d8dd9a8f7b0121ee403caf46e876ed
Alex C-G <alexcg@outlook.com>
2021-01-12 15:34:52 +0000

    test: show whole response

--- examples/jina_text.py
+++ examples/jina_text.py
@@ -6,4 +6,4 @@ st.title("Jina Text Search")
 st.markdown("Please run our [South Park Docker example](https://github.com/jina-ai/examples/tree/master/southpark-search#try-out-this-demo-by-yourself) to test out this search")
 st.markdown("`docker run -p 45678:45678 jinaai/hub.app.distilbert-southpark:latest`")
 
-jina.text_search(endpoint="http://0.0.0.0:45678/api/search")
+jina.text_search()
