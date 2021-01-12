f661701db68195c405ade3bc153579f9774819b9
Alex C-G <alexcg@outlook.com>
2021-01-12 17:14:24 +0000

    fix: text use wiki example, define endpoints

--- examples/jina_text.py
+++ examples/jina_text.py
@@ -2,8 +2,10 @@ import streamlit as st
 from streamlit_jina import jina
 st.set_page_config(page_title="Jina Text Search",)
 
+endpoint = "http://0.0.0.0:65481/api/search"
+
 st.title("Jina Text Search")
-st.markdown("Please run our [South Park Docker example](https://github.com/jina-ai/examples/tree/master/southpark-search#try-out-this-demo-by-yourself) to test out this search")
-st.markdown("`docker run -p 45678:45678 jinaai/hub.app.distilbert-southpark:latest`")
+st.markdown("Please run our [Wikipedia search example](https://github.com/alexcg1/jina-wikipedia-sentences) to test out this search")
+st.markdown("`docker run -p 65481:65481 jinahub/app.app.jina-wikipedia-sentences-30k:0.2.3-0.9.5`")
 
 jina.text_search()
