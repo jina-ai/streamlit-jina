f5ef49cd0416d30b06c671c2cda1396a68a0e86c
Alex C-G <alexcg@outlook.com>
2021-02-03 10:06:44 +0000

    fix(streamlit-jina): fix ports, wording, rm old dir

--- examples/jina_image.py
+++ examples/jina_image.py
@@ -2,10 +2,9 @@ import streamlit as st
 from streamlit_jina import jina
 st.set_page_config(page_title="Jina Image Search",)
 
-endpoint = "http://0.0.0.0:34567/api/search"
+endpoint = "http://0.0.0.0:45678/api/search"
 
 st.title("Jina Image Search")
-st.markdown("Please run our [Pokemon Docker example](https://github.com/jina-ai/examples/tree/master/pokedex-with-bit#tldr-just-show-me-the-pokemon) to test out this search")
-st.markdown("`docker run -p 34567:34567 -e \"JINA_PORT=34567\" jinaai/hub.app.bitsearch-pokedex search`")
+st.markdown("You can run Jina's [Pokemon Docker example](https://github.com/jina-ai/examples/tree/master/pokedex-with-bit#tldr-just-show-me-the-pokemon) to test out this search")
 
 jina.image_search(endpoint=endpoint)
--- examples/jina_text.py
+++ examples/jina_text.py
@@ -2,10 +2,9 @@ import streamlit as st
 from streamlit_jina import jina
 st.set_page_config(page_title="Jina Text Search",)
 
-endpoint = "http://0.0.0.0:65481/api/search"
+endpoint = "http://0.0.0.0:45678/api/search"
 
 st.title("Jina Text Search")
-st.markdown("Please run our [Wikipedia search example](https://github.com/alexcg1/jina-wikipedia-sentences) to test out this search")
-st.markdown("`docker run -p 65481:65481 jinahub/app.app.jina-wikipedia-sentences-30k:0.2.3-0.9.5`")
+st.markdown("You can run our [Wikipedia search example](https://github.com/jina-ai/examples/tree/master/wikipedia-sentences) to test out this search")
 
 jina.text_search()
