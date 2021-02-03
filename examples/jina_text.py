83cda7ed10c44516f7fe1ab21d56fe978b5d54cd
Alex C-G <alexcg@outlook.com>
2021-02-03 10:14:03 +0000

    fix(streamlit-jina): add optional endpoint param

--- examples/jina_text.py
+++ examples/jina_text.py
@@ -7,4 +7,4 @@ endpoint = "http://0.0.0.0:45678/api/search"
 st.title("Jina Text Search")
 st.markdown("You can run our [Wikipedia search example](https://github.com/jina-ai/examples/tree/master/wikipedia-sentences) to test out this search")
 
-jina.text_search()
+jina.text_search(endpoint=endpoint)
